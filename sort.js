4c11efa877e44d9eeba538f0d129cfc142eb8084
Abhishek <theabhishek.srm@gmail.com>
2024-07-25 22:36:22 +0530

    sort

---
+++ sort.js
@@ -0,0 +1,72 @@
+
+// let arr = ["apple", "Mango", "banana", "grapes"];
+
+
+// arr.sort(); 
+
+
+// console.log(arr); 
+
+
+// let s1 = "apple";
+// let s2 = "apPzzzzzzzzzzzzzzzzzzz";
+
+
+// console.log(s1>s2)
+
+
+// let arr = [100,2,15,10]
+       // ["1", "2", "15", "10"]
+
+// arr.sort(customLogic)
+
+
+// function customLogic(a,b){
+//      if(a>b){
+//           return 1
+//      }
+//      else if(a<b){
+//           return -1
+//      }
+// }
+
+// function customLogic(a,b){
+//         return a-b
+// }
+
+
+// positive number => swap
+// negative number => no swap
+
+
+
+
+
+
+// console.log(arr)
+
+
+
+// let s1= "2"
+// let s2= "15"
+
+// console.log(s2>s1) // false
+
+
+// let arr = ["apple", "mango", "banana", "grapes"]
+
+
+// arr.sort().reverse()
+
+// arr.sort(customLogic1)
+
+// function customLogic1(a,b){
+//     if(a>b){
+//        return -1 
+//     }
+//     else{
+//         return 1
+//     }
+// }
+
+// console.log(arr)
